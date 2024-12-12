b5425b80421a1ea23e1d2deac3323421c4bcfdd9
Karimmdi <karim.ahmadi@tuni.fi>
2024-12-12 14:11:22 +0200

    adding more functions to app

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e){
     e.preventDefault();
@@ -17,7 +19,10 @@ function addItem(e){
    li.appendChild(document.createTextNode(newItem));
     const button = createButton('remove-item btn-link text-red');
     li.appendChild(button);
+
+    //Add li to the DOM
     itemList.appendChild(li);
+    checkUI();
     itemInput.value = "";
 
 } 
@@ -35,7 +40,70 @@ function createIcon (classes){
    icon.className = classes;
    return icon;
 }
+//removeItem function
+function removeItem(e) {
+    if(e.target.parentElement.classList.contains('remove-item')){
+        if(confirm('Oletko varmaa?')){
+            e.target.parentElement.parentElement.remove();
 
+            checkUI();
+        }
+        
+    }
+
+
+}
+
+//clearItem function
+
+function clearItems(e){
+    // itemList.innerHTML = '';              *** easy way ***
+
+
+
+    //another approach 
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild);
+    } 
+    checkUI();
+
+    
+}
+//filterItems function
+function filterItems(e){
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+
+    items.forEach((item) =>{
+        const itemName = item.firstChild.textContent.toLocaleLowerCase();
+        if(itemName.indexOf(text)!= -1){
+            item.style.display = 'flex';
+
+        }else{
+            item.style.display = 'none';
+        }
+    });
+    
+
+}
+// function that check the UI if there is any items
+function checkUI(){
+    const items = itemList.querySelectorAll('li');
+    if(items.length === 0){
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }else{
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
+
+    }
+
+}
 //Event listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+checkUI();
 
